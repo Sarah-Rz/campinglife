2b43ca5696b212be851bdb5d401d9dd7ae5136ec
Sarah_RZ <rezaeisara1994@yahoo.com>
2023-08-12 16:28:58 +0330

    Navbar structure created

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import {
+  Box,
+  Flex,
+  Hstack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+  Button,
+  Stack,
+  useColorModeValue,
+  useColorMode,
+} from "@chakra-ui/react";
+import { Link as ReactLink } from "react-router-dom";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  MoonIcon,
+  SunIcon,
+  ReactIcon,
+} from "@chakra-ui/icons";
+import { GiCampingTent } from "react-icons/gi";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
